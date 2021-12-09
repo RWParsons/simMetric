dbea7d7db96e8896341eb81cb24c36da86e2260b
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2021-12-09 09:25:33 +0800

    coverage() typos and error handling

--- R/coverage.R
+++ R/coverage.R
@@ -2,33 +2,36 @@
 #'
 #' Calculate the coverage given a vector of confidence intervals and the true value
 #'
-#' @param true_value The true value which should be covered by the interval
-#' @param ll A numeric vector containing the lower limits of the confidence intervals
-#' @param ul A numeric vector containing the upper limits of the confidence intervals
-#' @param get a character vector containing the values returned by the function.
-#' @param na.rm a logical value indicating whether NA values for ll and ul should be removed before coverage estimation.
+#' @param true_value The true value which should be covered by the interval.
+#' @param ll A numeric vector containing the lower limits of the confidence intervals.
+#' @param ul A numeric vector containing the upper limits of the confidence intervals.
+#' @param get A character vector containing the values returned by the function.
+#' @param na.rm A logical value indicating whether NA values for ll and ul should be removed before coverage estimation.
 #'
 #' @return named vector containing the estimate and the monte carlo standard error for the coverage.
 #' @export
 #'
-#' @examples coverage(true_value=0, ll=rnorm(100, -1), ul=rnorm(100, 1))
+#' @examples coverage(true_value=0, ll=c(-1, -1, -1, -1), ul=c(1, 1, 1, -0.5))
 coverage <- function(true_value, ll, ul, get=c("coverage", "coverage_mcse"), na.rm=FALSE){
   assertthat::assert_that(length(ll) == length(ul))
-
+  assertthat::assert_that(length(!is.na(ul) & !is.na(ll)) > 0)
   x <- c()
-  covered <- true_value <= ul & true_value >= ll
 
-  if(all(is.na(covered))){
+  if(na.rm){
+    ul <- ul[!is.na(ul) & !is.na(ll)]
+    ll <- ll[!is.na(ul) & !is.na(ll)]
+  }
+
+  if(any(is.na(c(ul, ll)))){
     x["coverage"] <- NA
     x["coverage_mcse"] <- NA
     return(x[get])
   }
 
-  if(na.rm){
-    covered <- covered(!is.na(covered))
-  }
-
+  assertthat::assert_that(all(ll < ul))
+  covered <- true_value <= ul & true_value >= ll
   n <- length(covered)
+
   x["coverage"] <- mean(covered)
   x["coverage_mcse"] <- sqrt((x["coverage"]*(1-x["coverage"]))/n)
   return(x[get])
--- tests/testthat/test-coverage.R
+++ tests/testthat/test-coverage.R
@@ -3,16 +3,16 @@ test_that("multiplication works", {
 })
 
 test_that("coverage estimation", {
-  coverage_vec <- coverage(true_value=0, ll=c(rep(-1,9), 1), ul=c(rep(1,9), -1))
+  coverage_vec <- coverage(true_value=0, ll=c(rep(-1,9), 1), ul=c(rep(1,9), 2))
   expect_equal(coverage_vec[['coverage']], 0.9)
   expect_equal(coverage_vec[['coverage_mcse']], 0.09486833)
 })
 
 test_that("add a column to dataframe", {
   test_df <- data.frame(
-    ll=rnorm(100),
-    ul=rnorm(100),
-    group_id=sample(c("a", "b"), 100, replace=T)
+    ll=rnorm(20),
+    ul=rnorm(20, mean=5),
+    group_id=sample(c("a", "b"), 20, replace=T)
   )
   expect_silent({
     test_df$cov <- coverage(true_value=0, ll=test_df$ll, ul=test_df$ul, get=c("coverage"))
